8a04d3aba3538fda9376aac8582a1b24301b8103
DynaTiuM <raphael.perrin754@gmail.com>
2026-02-04 20:15:33 +0100

    feat: food specs

--- src/main/java/org/tact/features/hunger/config/HungerConfig.java
+++ src/main/java/org/tact/features/hunger/config/HungerConfig.java
@@ -29,25 +29,70 @@ public class HungerConfig {
     }
 
     private void initializeDefaultFoodValues() {
-        addFood("Ingredient_Dough", 2.0F, 0.0F);
-        addFood("Ingredient_Flour", 1.0F, 0.0F);
+        addFood("Ingredient_Dough", 5.0F, 0.0F);
+        addFood("Ingredient_Flour", 5.0F, -5.0F);
 
-        addFood("Plant_Fruit_Apple", 15.0F, 2.0F);
-        addFood("Plant_Fruit_Mango", 15.0F, 3.0F);
-        addFood("Plant_Fruit_Coconut", 15.0F, 2.0F);
+        addFood("Plant_Fruit_Mango", 15.0F, 20.0F);
+        addFood("Plant_Fruit_Coconut", 15.0F, 20.0F);
+        addFood("Plant_Fruit_Azure", 15.0F, 20.0F);
+        addFood("Plant_Fruit_Pinkberry", 15.0F, 20.0F);
+        addFood("Plant_Fruit_Poison", 15.0F, 20.0F);
+        addFood("Plant_Fruit_Spiral", 15.0F, 20.0F);
+        addFood("Plant_Fruit_Windwillow", 15.0F, 20.0F);
 
-        addFood("Plant_Crop_Potato_Item", 7.0F, 2.0F);
-        addFood("Plant_Crop_Carrot_Item", 8.0F, 2.0F);
-        addFood("Plant_Crop_Corn_Item", 10.0F, 2.0F);
+        addFood("Plant_Fruit_Apple", 10.0F, 5.0F);
+        addFood("Plant_Fruit_Berries_Red", 7.5F, 3.0F);
 
-        addFood("Food_Beef_Raw", 12.0F, 0.0F);
-        addFood("Food_Pork_Raw", 10.0F, 0.0F);
-        addFood("Food_Chicken_Raw", 8.0F, 0.0F);
+        addFood("Plant_Crop_Wheat_Item", 5.0F, 2.0F);
+        addFood("Plant_Crop_Lettuce_Item", 5.0F, 2.0F);
+
+        addFood("Plant_Crop_Corn_Item", 7.5F, 3.0F);
+        addFood("Plant_Crop_Potato_Item", 7.5F, 3.0F);
+        addFood("Plant_Crop_Carrot_Item", 7.5F, 3.0F);
+        addFood("Plant_Crop_Aubergine_Item", 7.5F, 3.0F);
+        addFood("Plant_Crop_Cauliflower_Item", 7.5F, 3.0F);
+        addFood("Plant_Crop_Turnip_Item", 7.5F, 3.0F);
+        addFood("Plant_Crop_Pumpkin_Item", 7.5F, 3.0F);
+        addFood("Plant_Crop_Tomato_Item", 7.5F, 3.0F);
+        addFood("Plant_Crop_Chili_Item", 7.5F, 3.0F);
+        addFood("Plant_Crop_Rice_Item", 7.5F, 3.0F);
+
+        addFood("Food_Beef_Raw", 7.5F, -5.0F);
+        addFood("Food_Pork_Raw", 7.5F, -5.0F);
+        addFood("Food_Chicken_Raw", 7.5F, -5.0F);
+        addFood("Food_Wildmeat_Raw", 7.5F, -5.0F);
+
+        addFood("Food_Wildmeat_Cooked", 17.5F, 5.0F);
+
+        addFood("Food_Fish_Raw", 5.5F, -10.0F);
+        addFood("Food_Fish_Grilled", 15.5F, 5.0F);
 
         addFood("Food_Bread", 20.0F, 5.0F);
-        addFood("Food_Wildmeat_Cooked", 35.0F, 5.0F);
-        addFood("Food_Kebab_Meat", 45.0F, 15.0F);
+
+        addFood("Food_Kebab_Mushroom", 40.0F, 8.0F);
+        addFood("Food_Kebab_Fruit", 40.0F, 8.0F);
+        addFood("Food_Kebab_Meat", 60.0F, 10.0F);
+        addFood("Food_Kebab_Vegetable", 30.0F, 8.0F);
+
+        addFood("Food_Salad_Mushroom", 35.0F, 15.0F);
+        addFood("Food_Salad_Berry", 35.0F, 15.0F);
+
+
+        addFood("Food_Vegetable_Cooked", 15.0F, 5.0F);
+
         addFood("Food_Pie_Meat", 65.0F, 15.0F);
+        addFood("Food_Pie_Apple", 65.0F, 15.0F);
+        addFood("Food_Pie_Pumpkin", 65.0F, 15.0F);
+
+        addFood("Food_Cheese", 10.0F, 10.0F);
+        addFood("Food_Popcorn", 15.0F, 10.0F);
+
+        addFood("Ingredient_Spices", 5.0F, 5.0F);
+        addFood("Ingredient_Salt", 5.0F, -10.0F);
+
+        addFood("Food_Candy_Cane", 10.0F, 15.0F);
+        addFood("Food_Egg", 7.5F, 3F);
+
     }
 
     private void addFood(String id, float hunger, float comfort) {
